954ba8c29965616990fd133a591d6da9d127b691
Jobayer98 <joobayerrahman0220@gmail.com>
2024-06-01 06:49:23 +0600

    added Product model

--- InviteFashionAPI/models.py
+++ InviteFashionAPI/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    brand = models.ForeignKey(Brand, on_delete=models.SET_DEFAULT, blank=True, null=True, default=None)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return self.title
