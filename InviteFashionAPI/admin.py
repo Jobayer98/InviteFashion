88d16d8ef08a87221fea9c4adefdf3c7127b3067
Jobayer98 <joobayerrahman0220@gmail.com>
2024-06-01 07:45:25 +0600

    added product item model

--- InviteFashionAPI/admin.py
+++ InviteFashionAPI/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Brand, Category, Product, SubCategory, Size, Color, ProductItem, Review
+
+admin.site.register(Brand)
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(Product)
+admin.site.register(ProductItem)
+admin.site.register(Size)
+admin.site.register(Color)
+admin.site.register(Review)
--- InviteFashionAPI/models.py
+++ InviteFashionAPI/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+    
+class SubCategory(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self) -> str:
@@ -15,10 +22,47 @@ class Brand(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255, blank=True, null=True)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
-    brand = models.ForeignKey(Brand, on_delete=models.SET_DEFAULT, blank=True, null=True, default=None)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, related_name='products')
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.SET_NULL, blank=True, null=True)
+    brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, blank=True, null=True)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
     
     def __str__(self) -> str:
         return self.title
+    
+class ProductItem(models.Model):
+    original_price = models.DecimalField(max_digits=6, decimal_places=2)
+    sale_price = models.DecimalField(max_digits=6, decimal_places=2)
+    product_code = models.CharField(max_length=100, blank=True, null=True)
+    image_url = models.URLField()
+    quantity_in_stock = models.PositiveIntegerField()
+    porduct = models.ForeignKey(Product, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return f"{self.porduct.title} | {self.product_code}"
+    
+class Review(models.Model):
+    product_item = models.ForeignKey(ProductItem, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=255, blank=True, null=True)
+    rating = models.FloatField(validators=[MinValueValidator(1.0), MaxValueValidator(5.0)])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+            return f"{self.pk} | {self.menu_item.title} | {self.rating}"
+        
+class Size(models.Model):
+    size = models.CharField(max_length=5)
+    
+    def __str__(self) -> str:
+        return self.size
+    
+class Color(models.Model):
+    color = models.CharField(max_length=100)
+    
+    def __str__(self) -> str:
+        return self.color
+    
