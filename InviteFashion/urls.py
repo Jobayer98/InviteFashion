b0d70239c3a5115be5c948d60140023a9abc9112
Jobayer Rahman <109141956+Jobayer98@users.noreply.github.com>
2024-09-29 11:12:44 +0600

    refactor url

--- InviteFashion/urls.py
+++ InviteFashion/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/admin/', include('AdminAPI.urls')),
-    path('api/', include('InviteFashionAPI.urls')),
+    path('api/v1/admin/', include('AdminAPI.urls')),
+    path('api/v1/', include('InviteFashionAPI.urls')),
     path('admin/', admin.site.urls),
 ]
